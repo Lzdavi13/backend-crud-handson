7084aaea71aeb19b69ab5ee0d03b073f4874ad73
Luiz D O Santos <lzdavi1325@gmail.com>
2024-02-21 15:30:50 -0300

    feat: add data validation to create and update routes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,16 @@
-import { IsEmail, IsEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmpty({ message: 'Por favor preencha todos os campos' })
+  @IsNotEmpty({ message: 'Por favor preencha todos os campos' })
   @IsString()
   name: string;
 
   @IsEmail()
-  @IsEmpty({ message: 'Por favor preencha todos os campos' })
+  @IsNotEmpty({ message: 'Por favor preencha todos os campos' })
   @IsString()
   email: string;
 
-  @IsEmpty({ message: 'Por favor preencha todos os campos' })
+  @IsNotEmpty({ message: 'Por favor preencha todos os campos' })
   @IsString()
   password: string;
 }
